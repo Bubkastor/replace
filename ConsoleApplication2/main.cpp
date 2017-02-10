7b4251051c697ce0bae9ae610a1de8f1262b5b55
bubok <bubok@DESKTOP-EMC4A6P>
2017-02-11 01:56:18 +0300

    add all file add config

--- ConsoleApplication2/main.cpp
+++ ConsoleApplication2/main.cpp
@@ -2,12 +2,21 @@
 #include <fstream>
 #include <string>
 #include <regex>
-
+#include "Lab.h"
 
 using namespace std;
 
 void main(int argc, char* argv[])
 {
 	// inputFile outputFile search replace
-	if(argc != 3)
+
+	Lab *lab;
+	if (argc >= 4) {
+		lab = new Lab(argv[1], argv[2], argv[3], argv[4]);
+	}
+	else
+	{
+		std::cout << "Not all args" << std::endl;
+	}
+	lab->Run();
 }
